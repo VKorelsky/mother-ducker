b45b4d7baa6f11436837b8e7b176defcbedc3759
Victor Korelsky <victor.korelsky@mail.mcgill.ca>
2018-07-14 09:52:58 -0400

    cabling

--- lib/mother_ducker.rb
+++ lib/mother_ducker.rb
@@ -1,6 +1,6 @@
 class MotherDucker
-  # test to see if it works 
-  self.works?
-    true
+  # test to see if it works
+  def self.works?
+    puts "yo, i have been SUMMONED YEAHHHHHHHHHHHHHHH"
   end
 end
