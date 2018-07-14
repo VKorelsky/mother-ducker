74f361b9d676fe265b0e4f7a767ff29abdc4eed3
Victor Korelsky <victor.korelsky@mail.mcgill.ca>
2018-07-14 15:10:43 -0400

    fix typo

--- lib/mother_ducker/constants.rb
+++ lib/mother_ducker/constants.rb
@@ -1,9 +1,9 @@
 module MotherDucker
-    TEXT_ARRAY = [ "Get comfortable",
-                   "Relax your body by releasing any areas of tension",
-                   "You're at a hackathon... You've been coding for hours...",
-                   "Forget about bugs... Forget about method errors...",
-                   "Close your eyes...",
-                   "Now follow the animation on the screen.. breathe in when it expands, out when it contracts"
-                 ]
+    MEDITATION_TEXT = [ "Get comfortable",
+                       "Relax your body by releasing any areas of tension",
+                       "You're at a hackathon... You've been coding for hours...",
+                       "Forget about bugs... Forget about method errors...",
+                       "Close your eyes...",
+                       "Now follow the animation on the screen.. breathe in when it expands, out when it contracts"
+                     ]
 end
