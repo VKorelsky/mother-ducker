a056419d8a6619bc28a5a65650e7f833207e3eb5
Aaron Mednick <aaronjosephm@gmail.com>
2018-07-14 13:53:09 -0400

    Finished rubber duckie.

--- debugger.rb
+++ debugger.rb
@@ -1,17 +1,23 @@
-require "pry"
-
 def error_parser(message)
 
   if message.match(/undefined method/)
     line = /\:\d+\:/.match(message)[0].gsub(":", "")
-    file_name = /\#\<\w+\:/.match(message)[0].gsub("#<", "").gsub(":", "").downcase
+    file_name = /\#\<\w+\:/.match(message)[0].gsub("#<", "").gsub(":", "")
     puts "\n"
-    puts "Looks like we have an undefined method on line #{line} of your #{file_name}.rb file.\n  Do you know what this means?"
+    puts "Looks like we have an undefined method on line #{line} of your #{file_name.downcase}.rb file.\n  Do you know what this means?"
     answer_1 = gets.chomp
     if answer_1 == "yes"
       puts "Then go to line #{line} and fix it..."
     else
+      puts "You haven't properly defined a method in your #{file_name} class."
       puts "Let's go to line #{line} of your code, and see what's wrong."
+      puts "Which method is being called on line #{line}?"
+      method_name = gets.chomp
+      puts "Is there a method defined \"#{method_name}\" in your #{file_name} class?"
+      answer_2 = gets.chomp
+      if answer_2 == "no"
+        puts "Create a method named \"#{method_name}\" in your #{file_name} class."
+      end
     end
   end
 
