5718dba84e17dd580e3f0fa9f13b6303b1db582b
Victor Korelsky <victor.korelsky@mail.mcgill.ca>
2018-07-14 13:56:08 -0400

    o

--- lib/mother_ducker/strategy_orchestrator.rb
+++ lib/mother_ducker/strategy_orchestrator.rb
@@ -2,11 +2,29 @@ module MotherDucker
   class StrategyCoordinator
     def initialize(user_instance)
       @user = user_instance
+      @meditation_used = false
+      @debugging_used = false
     end
 
     def strategize
       # TODO
-      puts "Code me ! StrategyCoordinator#strategize! I will strategize"
+      if !(@meditation_used)
+        meditate
+      elsif !(@debugging_used)
+        debug
+      else
+        puts "we did what we could ! maybe take a nap ? or look at memes?"
+      end
+    end
+
+    def meditate
+
+      @meditation_used = true
+    end
+
+    def debug
+
+      @debugging_used = false
     end
   end
 end
