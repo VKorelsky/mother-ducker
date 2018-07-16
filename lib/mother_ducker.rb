eef9f346c8a0ad94bba5e20bb0fe3e424801731d
Victor <VKorelsky@users.noreply.github.com>
2018-07-16 18:35:49 -0400

    update

--- lib/mother_ducker.rb
+++ lib/mother_ducker.rb
@@ -2,13 +2,10 @@
 require_relative "mother_ducker/constants"
 require_relative "mother_ducker/strategy_orchestrator"
 
-# TODO: change name of this class
-# class in module in charge of running the code
 module MotherDucker
     class Starter
         def self.launch
             satisfied = false
-            # initiate a strategy class, passing it the user instance
             strategy_orchestrator = StrategyCoordinator.new
 
             puts "Welcome to the MotherDucker ! I will help you relax and debug your code"
@@ -18,9 +15,8 @@ module MotherDucker
             gets.chomp
             puts "Ok ! Let's work through that"
 
-            # call Strategy.strategize while user.satisfied == false
             until satisfied
-              # this method asks the user if he is happy currently ?
+              # apply a strategy to help the user debug
               strategy_orchestrator.strategize
               puts ""
               puts "are you feeling better ? [ y | n ]"
@@ -28,7 +24,6 @@ module MotherDucker
               input = gets.chomp.downcase
               satisfied = true if (input == "y" || input == "yes")
             end
-            # it will pick and execute a strategy from the set
 
             puts "thanks for using me! see you soon !"
         end
